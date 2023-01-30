b2cf81b9cc8d7d7ca0b2866a7bd7dc9b55e32a70
mandakh <mmandah308@gmail.com>
2023-01-30 14:05:10 +0800

    asdf

--- src/main/MainHeader.js
+++ src/main/MainHeader.js
@@ -9,13 +9,7 @@ import {
   Spin,
   Drawer,
   Badge,
-  Card,
-  Popconfirm,
-  message,
-  Empty,
-  Avatar,
 } from "antd";
-import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
 import PropTypes from "prop-types";
 import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
@@ -24,13 +18,12 @@ import {
   ShoppingCartOutlined,
   UserOutlined,
   LoadingOutlined,
-  TeamOutlined,
   CloseOutlined,
-  CarOutlined,
 } from "@ant-design/icons";
 import contextLogin from "./contextLogin";
 import adminService from "../services/adminService";
-import { moneyFormat, showErrorMsg } from "../common/utils";
+import { showErrorMsg } from "../common/utils";
+import OrderDrawer from "./OrderDrawer";
 
 const MainHeader = ({ userLoading }) => {
   const {
@@ -55,7 +48,7 @@ const MainHeader = ({ userLoading }) => {
     setOpen(false);
   };
 
-  const getOrder = (e) => {
+  const getOrder = () => {
     setLoading(true);
     adminService
       .getOrderDetail({ status: 0 })
@@ -70,22 +63,6 @@ const MainHeader = ({ userLoading }) => {
       })
       .catch((err) => showErrorMsg(err))
       .finally(() => setLoading(false));
-  };
-
-  const deleteProductFromOrder = (dtl_id, order_id) => {
-    setLoading(true);
-    adminService
-      .deleteOrderDtl({ id: dtl_id, order_id: order_id })
-      .then((result) => {
-        if (result?.data) {
-          getOrder(1);
-          message.success("Амжилттай устгагдлаа");
-        }
-      })
-      .catch((err) => {
-        showErrorMsg(err);
-        setLoading(false);
-      });
   };
 
   React.useEffect(() => {
@@ -152,37 +129,7 @@ const MainHeader = ({ userLoading }) => {
       ]}
     />
   );
-  const items = [
-    {
-      label: "Admin",
-      key: "admin",
-      icon: <TeamOutlined />,
-    },
-  ];
-  const itemsDist = [
-    {
-      label: "Түгээлт",
-      key: "dist_driver",
-      icon: <CarOutlined />,
-    },
-  ];
 
-  const getDrawerAvatar = (item) => {
-    if (item.type === 1 && item.productFilename)
-      return (
-        <Avatar
-          src={`${process.env.REACT_APP_SERVICE_URL}/images/${item.productFilename}`}
-        />
-      );
-    if (item.packageFilename)
-      return (
-        <Avatar
-          src={`${process.env.REACT_APP_SERVICE_URL}/images/${item.packageFilename}`}
-        />
-      );
-
-    return <Avatar src={`/images/emptyPic.jpeg`} />;
-  };
   return (
     <Spin spinning={loading}>
       <Row justify="space-between">
@@ -309,94 +256,11 @@ const MainHeader = ({ userLoading }) => {
           open={open}
         >
           {open && (
-            <Spin spinning={loading}>
-              {scheduleOrder?.detailList ? (
-                <Row gutter={[0, 16]}>
-                  {scheduleOrder.detailList.map((p) => {
-                    return (
-                      <Col span={24} key={p.id}>
-                        <Card key={p.id}>
-                          <Row
-                            justify="space-between"
-                            style={{ marginBottom: "2px" }}
-                          >
-                            <Col>{getDrawerAvatar(p)}</Col>
-                            <Col>
-                              <Popconfirm
-                                title="Сагснаас устгахдаа итгэлтэй байна уу?"
-                                placement="topRight"
-                                onConfirm={() =>
-                                  deleteProductFromOrder(p.id, scheduleOrder.id)
-                                }
-                              >
-                                <Button
-                                  type="primary"
-                                  danger
-                                  size="small"
-                                  ghost
-                                  icon={<CloseOutlined />}
-                                />
-                              </Popconfirm>
-                            </Col>
-                          </Row>
-                          <Row justify="space-between">
-                            <Col>Нэр: </Col>
-                            <Col>{p.product ?? p.package}</Col>
-                          </Row>
-                          <Row justify="space-between">
-                            <Col>Үнэ: </Col>
-                            <Col>{moneyFormat(p.price)}</Col>
-                          </Row>
-                          <Row justify="space-between">
-                            <Col>Тоо: </Col>
-                            <Col>
-                              <Space>
-                                <Button
-                                  icon={<MinusOutlined />}
-                                  onClick={() => {}}
-                                  size="small"
-                                />
-                                {p.type === 1 ? p.qty + "кг" : p.qty + "ш"}
-                                <Button
-                                  icon={<PlusOutlined />}
-                                  onClick={() => {}}
-                                  size="small"
-                                />
-                              </Space>
-                            </Col>
-                          </Row>
-                          <Row justify="space-between">
-                            <Col>Нийт үнэ: </Col>
-                            <Col>{moneyFormat(p.amount)}</Col>
-                          </Row>
-                        </Card>
-                      </Col>
-                    );
-                  })}
-                  <Col span={24}>
-                    <Row justify="space-between">
-                      <Col>
-                        Нийт дүн:{" "}
-                        <b>{moneyFormat(scheduleOrder.total_amount)}</b>
-                      </Col>
-                      <Col>
-                        <Button
-                          type="primary"
-                          onClick={() => {
-                            history.push("/order");
-                            onClose();
-                          }}
-                        >
-                          Баталгаажуулах
-                        </Button>
-                      </Col>
-                    </Row>
-                  </Col>
-                </Row>
-              ) : (
-                <Empty description="Сагс хоосон байна!" />
-              )}
-            </Spin>
+            <OrderDrawer
+              scheduleOrder={scheduleOrder}
+              getOrder={getOrder}
+              onClose={onClose}
+            />
           )}
         </Drawer>
       </Row>
---
+++ src/main/OrderDrawer.js
@@ -0,0 +1,152 @@
+import {
+  Col,
+  Row,
+  Button,
+  Space,
+  Spin,
+  Card,
+  Popconfirm,
+  message,
+  Empty,
+  Avatar,
+} from "antd";
+import { MinusOutlined, PlusOutlined } from "@ant-design/icons";
+import PropTypes from "prop-types";
+import React, { useState } from "react";
+import { useHistory } from "react-router-dom";
+import { CloseOutlined } from "@ant-design/icons";
+import adminService from "../services/adminService";
+import { moneyFormat, showErrorMsg } from "../common/utils";
+
+const OrderDrawer = ({ getOrder, scheduleOrder, onClose }) => {
+  let history = useHistory();
+
+  const [loading, setLoading] = useState(false);
+
+  const deleteProductFromOrder = (dtl_id, order_id) => {
+    setLoading(true);
+    adminService
+      .deleteOrderDtl({ id: dtl_id, order_id: order_id })
+      .then((result) => {
+        if (result?.data) {
+          getOrder();
+          message.success("Амжилттай устгагдлаа");
+        }
+      })
+      .catch((err) => {
+        showErrorMsg(err);
+      })
+      .finally(() => setLoading(false));
+  };
+
+  const getDrawerAvatar = (item) => {
+    if (item.type === 1 && item.productFilename)
+      return (
+        <Avatar
+          src={`${process.env.REACT_APP_SERVICE_URL}/images/${item.productFilename}`}
+        />
+      );
+    if (item.packageFilename)
+      return (
+        <Avatar
+          src={`${process.env.REACT_APP_SERVICE_URL}/images/${item.packageFilename}`}
+        />
+      );
+
+    return <Avatar src={`/images/emptyPic.jpeg`} />;
+  };
+
+  return (
+    <Spin spinning={loading}>
+      {scheduleOrder?.detailList ? (
+        <Row gutter={[0, 16]}>
+          {scheduleOrder.detailList.map((p) => {
+            return (
+              <Col span={24} key={p.id}>
+                <Card key={p.id}>
+                  <Row justify="space-between" style={{ marginBottom: "2px" }}>
+                    <Col>{getDrawerAvatar(p)}</Col>
+                    <Col>
+                      <Popconfirm
+                        title="Сагснаас устгахдаа итгэлтэй байна уу?"
+                        placement="topRight"
+                        onConfirm={() =>
+                          deleteProductFromOrder(p.id, scheduleOrder.id)
+                        }
+                      >
+                        <Button
+                          type="primary"
+                          danger
+                          size="small"
+                          ghost
+                          icon={<CloseOutlined />}
+                        />
+                      </Popconfirm>
+                    </Col>
+                  </Row>
+                  <Row justify="space-between">
+                    <Col>Нэр: </Col>
+                    <Col>{p.product ?? p.package}</Col>
+                  </Row>
+                  <Row justify="space-between">
+                    <Col>Үнэ: </Col>
+                    <Col>{moneyFormat(p.price)}</Col>
+                  </Row>
+                  <Row justify="space-between">
+                    <Col>Тоо: </Col>
+                    <Col>
+                      <Space>
+                        <Button
+                          icon={<MinusOutlined />}
+                          onClick={() => {}}
+                          size="small"
+                          type="link"
+                          ghost
+                        />
+                        {p.type === 1 ? p.qty + "кг" : p.qty + "ш"}
+                        <Button
+                          icon={<PlusOutlined />}
+                          type="link"
+                          onClick={() => {}}
+                          size="small"
+                        />
+                      </Space>
+                    </Col>
+                  </Row>
+                  <Row justify="space-between">
+                    <Col>Нийт үнэ: </Col>
+                    <Col>{moneyFormat(p.amount)}</Col>
+                  </Row>
+                </Card>
+              </Col>
+            );
+          })}
+          <Col span={24}>
+            <Row justify="space-between">
+              <Col>
+                Нийт дүн: <b>{moneyFormat(scheduleOrder.total_amount)}</b>
+              </Col>
+              <Col>
+                <Button
+                  type="primary"
+                  onClick={() => {
+                    onClose();
+                    history.push("/order");
+                  }}
+                >
+                  Баталгаажуулах
+                </Button>
+              </Col>
+            </Row>
+          </Col>
+        </Row>
+      ) : (
+        <Empty description="Сагс хоосон байна!" />
+      )}
+    </Spin>
+  );
+};
+
+OrderDrawer.propTypes = {};
+// export default withRouter(MainHeader);
+export default OrderDrawer;
